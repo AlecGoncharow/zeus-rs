4169c024fd6f1f1e5e67b034997811f7e78efd07
Alec Goncharow <Alec.Goncharow@gmail.com>
2021-03-22 23:46:01 -0400

    pull in more code, whoops haha

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use common::{Complex, CustomMsg};
+use hermes::message::Message;
+use hermes::server::ServerInterface;
+
+#[tokio::main]
+async fn main() {
+    let mut server: ServerInterface<CustomMsg> = ServerInterface::new(8080);
+    server.start().await;
+    let mut connection_count = 0;
+
+    loop {
+        server.update().await;
+        let curr_connection_count = server.connection_count();
+        if connection_count != curr_connection_count {
+            println!(
+                "[Driver] change in connection count: old:{} new:{}",
+                connection_count, curr_connection_count
+            );
+            let ping = Message::new(CustomMsg::Ping);
+            server.send_to_all(ping).await;
+            connection_count = curr_connection_count;
+        }
+
+        if let Some(mut msg) = server.pop_message() {
+            println!("popped msg: {:?}", msg.header);
+            match msg.header.id {
+                CustomMsg::Player(_) => {}
+                CustomMsg::Ping => {}
+                CustomMsg::Interact(_) => {}
+                CustomMsg::MovePlayer(_) => {
+                    let parse: Complex = msg.pull();
+                    println!("parsed bytes for MovePlayer: {:#?}", parse);
+                }
+            }
+        }
+    }
 }
